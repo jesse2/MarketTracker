b7cec82494026cc5e4b9faa3b03cfe7966437b5d
jesse <jesse9726@gmail.com>
2017-04-08 20:02:16 -0700

    update

---
+++ theshow/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace theshow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(43, 1);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(163, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(5, 43);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1267, 674);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(212, -1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(336, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Buy Now:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(386, 1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(337, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Sell Now:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(386, 14);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(309, 27);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Profit After Fee:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(386, 27);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1, 4);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Player:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1197, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Add Player";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1271, 719);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Market Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ theshow/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace theshow
+{
+    public partial class Form1 : Form
+    {
+        List<play> players;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        struct play
+        {
+            private String name;
+            private String id;
+            public String N
+            {
+                get
+                {
+                    return name;
+                }
+                set
+                {
+                    name = value;
+                }
+            }
+
+            public String I
+            {
+                get
+                {
+                    return id;
+                }
+                set
+                {
+                    id = value;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("theshownation.com/sessions/login");
+            comboBox1.SelectedIndex = -1;
+            players = new List<play>();
+            getPlayers();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String ur = "http://theshownation.com/marketplace/listing?item_ref_id=";
+
+            String url = ur+players.ElementAt(comboBox1.SelectedIndex).I;
+            webBrowser1.Navigate(url);
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (webBrowser1.Url.ToString().Contains("http://theshownation.com/marketplace/listing?item_ref_id="))
+            {
+                String site = webBrowser1.DocumentText;
+                using (StreamWriter f = File.AppendText("code.txt"))
+                {
+                    f.WriteLine(site);
+                }
+                int index = 0;
+                int index2 = 0;
+                String temp="<INPUT name=\"price\" type=\"hidden\" value=\"";
+                if(site.Contains(temp))
+                {
+                    index = site.IndexOf(temp);
+                }
+                String tempstring = site.Substring(index, 50);
+                String buynow = tempstring.Substring(41,((tempstring.IndexOf(">")-1)-41));
+                label2.Text = buynow;
+                String newstring = site.Substring((index + temp.Length+buynow.Length+620), 150);
+                if (newstring.Contains(temp))
+                {
+                    index2 = newstring.IndexOf(temp);
+                }
+                String tempstring2 = newstring.Substring(index2, 50);
+                String sellprice = tempstring2.Substring(41, ((tempstring2.IndexOf(">") - 1) - 41));
+                label4.Text = sellprice;
+                double profit = double.Parse(buynow) - double.Parse(sellprice) - (double.Parse(buynow) * .1);
+                label6.Text = profit.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.Url.ToString().Contains("http://theshownation.com/marketplace/listing?item_ref_id="))
+            {                              
+                String id = webBrowser1.Url.ToString().Substring((webBrowser1.Url.ToString().Length - 5), 5);
+                id = id.Replace(" ", String.Empty);             
+                String site = webBrowser1.DocumentText;
+                StringReader read = new StringReader(site);
+                String line;
+                while (true)
+                {
+                    line = read.ReadLine();
+                    if(line.Contains("<H2>#"))
+                    {
+                        break;
+                    }
+                }
+                String name=line.Substring(9, (line.IndexOf(" </H") - 9));
+                int index = players.FindIndex(f => f.I == id);
+                if (index < 0)
+                {
+                    using (StreamWriter w = File.AppendText("players.txt"))
+                    {
+                        w.WriteLine(name + ", " + id);
+                    }
+                    comboBox1.Items.Clear();
+                    getPlayers();
+                }                
+            }
+        }
+
+        private void getPlayers()
+        {
+            players.Clear();
+            using (FileStream r = File.Open("players.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                if (r.Length > 0)
+                {
+                    using (StreamReader s = new StreamReader(r, true))
+                    {
+                        String line;
+                        while ((line = s.ReadLine()) != null)
+                        {
+                            String name = line.Substring(0, line.IndexOf(","));
+                            String id = line.Substring(name.Length + 2, 5);
+                            play temp = new play();
+                            temp.N = name;
+                            temp.I = id;
+                            players.Add(temp);
+                        }
+                    }
+                }
+            }
+            foreach(play player in players)
+            {
+                comboBox1.Items.Add(player.N);
+            }
+        }
+    }
+}
