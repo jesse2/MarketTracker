267c63b4c3385f981d757b8323893c1df102df87
jesse <jesse9726@gmail.com>
2017-04-09 23:14:46 -0700

    fixed form size

--- theshow/Form1.Designer.cs
+++ theshow/Form1.Designer.cs
@@ -294,7 +294,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1369, 680);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1268, 680);
             this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.textBox3);
